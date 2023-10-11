1d2f9cb661ae63a0801788908cd2256105178581
unknown <nilsonjosemoreirafilho@gmail.com>
2023-10-11 15:47:57 -0300

    Commit inicial

---
+++ src/main/java/br/com/njf/demofix/DemofixApplication.java
@@ -0,0 +1,13 @@
+package br.com.njf.demofix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemofixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemofixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/njf/demofix/DemofixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.njf.demofix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemofixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
